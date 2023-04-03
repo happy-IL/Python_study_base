e4c5837d2bc26022048c6af0e2d11e71d6530e97
ILYA VEDERNIKOV <118388201+happy-IL@users.noreply.github.com>
2023-04-03 21:18:36 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+Напишите программу, которая на вход принимает два числа A и B, 
+# и возводит число А в целую степень B с помощью рекурсии.
+
+def power(num1, num2):
+    if num2 == 0:
+        return 1
+
+    return num1 * power(num1, num2 - 1)
+
+
+num = power(int(input('number: ')), int(input('degree: ')))
+print(f'number is {num}')
